abeb3891b4ebd66ff0489fe0142f03d30945f494
SkyfallWasTaken <mahadkalam1234@gmail.com>
2022-10-12 21:39:47 +0100

    Add docs to command args

--- src/cli.rs
+++ src/cli.rs
@@ -7,19 +7,24 @@ pub struct Cli {
     pub command: Command,
 }
 
-/// Doc comment
 #[derive(Subcommand)]
 pub enum Command {
     /// Run a script in `package.json`.
     #[clap(aliases = &["run-script", "rum", "urn"])]
     Run {
+        /// The name of the script you want to run
         script: Option<String>,
+
+        /// Any arguments you want to pass to the script
         args: Option<Vec<String>>,
     },
 
     /// Test a package.
     Test {
+        /// The name of the script you want to run
         script: Option<String>,
+
+        /// Any arguments you want to pass to the script
         args: Option<Vec<String>>,
     },
 
@@ -31,6 +36,7 @@ pub enum Command {
     #[clap(aliases = &["create", "innit"])]
     Init {
         #[arg(short, long)]
+        /// Skip any prompts and just use the defaults
         yes: bool,
     },
 }
